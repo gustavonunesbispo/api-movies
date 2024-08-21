fdb42986cc993997e36efa0d9907e2b1394a026d
gustavo <gustavo.nbispo@outlook.com>
2024-08-21 17:25:10 -0300

    create update show request

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,5 +7,7 @@ const usersRoutes = Router();
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
+usersRoutes.get("/:id", usersController.show);
 
 module.exports = usersRoutes;
